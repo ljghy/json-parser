c42649f1639fc9f94b148883f31dcf47c1c8b7a5
ljghy <2436496086@qq.com>
2023-09-19 12:50:11 +0800

    update test

--- test.cpp
+++ test.cpp
@@ -1,34 +1,28 @@
-#include <filesystem>
-#include <iostream>
-#include <fstream>
-#include <sstream>
 #include "JsonParser.hpp"
+#include <filesystem>
+#include <fstream>
+#include <iostream>
+#include <sstream>
 
-int main()
-{
-    JsonParser parser;
-    JsonNode json;
-    std::string dir = "tests/JSONTestSuite/test_parsing/";
-    for (const auto &dirEntry : std::filesystem::directory_iterator(dir))
-    {
-        std::stringstream ss;
-        ss << dirEntry;
-        bool success = true;
-        std::string filename = dir + ss.str().substr(dir.size() + 1, ss.str().size() - dir.size() - 2);
-        try
-        {
-            std::ifstream fin(filename, std::ios::binary);
-            json = parser.parse(fin);
-        }
-        catch (const std::exception &e)
-        {
-            std::cerr << "Fail: " << filename << ": " << e.what() << '\n';
-            success = false;
-        }
-        if (success)
-        {
-            std::cerr << "Success: " << filename << "\n";
-            std::cerr << json.toString() << '\n';
-        }
+int main() {
+  JsonParser parser;
+  JsonNode json;
+  std::string dir = "tests/JSONTestSuite/test_parsing/";
+  for (const auto &dirEntry : std::filesystem::directory_iterator(dir)) {
+    std::stringstream ss;
+    ss << dirEntry;
+    bool success = true;
+    std::string filename =
+        ss.str().substr(dir.size() + 1, ss.str().size() - dir.size() - 2);
+    std::string path = dir + filename;
+    try {
+      std::ifstream fin(path, std::ios::binary);
+      json = parser.parse(fin);
+    } catch (const std::exception &e) {
+      success = false;
     }
+    if ((success && filename[0] == 'n') || (!success && filename[0] == 'y')) {
+      std::cerr << "Unexpected parsing result: " << filename << '\n';
+    }
+  }
 }
